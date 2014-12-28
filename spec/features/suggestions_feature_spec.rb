8c525677f613b7cc0b0d7e09a471f4a646a12097
chrisjbatts <chrisjbatts@gmail.com>
2014-12-28 13:20:16 +0000

    suggestion(s) conflict fixed

--- spec/features/suggestions_feature_spec.rb
+++ spec/features/suggestions_feature_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 feature 'suggestions' do
 
   context 'no suggestions have been added' do
-
-    scenario 'should prompt the visitor to register and add a suggestion' do
-      visit '/'
-      expect(page).to have_content 'No suggestions have been added yet'
-      expect(page).to have_link 'Register and add a suggestion'
+    
+    scenario 'should display a prompt to add a suggestion' do
+      visit '/suggestions'
+      expect(page).to have_content 'No suggestions'
+      expect(page).to have_link 'Add a suggestion'
     end
-  
+
   end
 
 end
